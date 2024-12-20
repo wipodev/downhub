37c3003712f9603282b9a84f78d7d9a5cc648bb8
wipodev <ajwipo@gmail.com>
2024-12-19 20:11:58 -0500

    added endpoint for downloading youtube videos

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,19 +1,38 @@
-from fastapi import FastAPI
+# python modules
+import os
+
+# third-party modules
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from yt_dlp import YoutubeDL
 
 app = FastAPI()
 
-# Permitir CORS para que la extensión de Chrome pueda hacer solicitudes
+# Allow CORS so the Chrome extension can make requests
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Puedes especificar dominios específicos en lugar de '*'
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.get("/data")
-async def get_data():
-    return {"message": "¡Hola desde el backend en FastAPI!"}
+class VideoRequest(BaseModel):
+    url: str
 
-# Ejecuta el servidor con: uvicorn main:app --reload
+@app.post("/download")
+def download_video(request: VideoRequest):
+    downloads_folder = os.path.join(os.path.expanduser("~"), "Downloads")
+    
+    ydl_opts = {
+        'outtmpl': os.path.join(downloads_folder, '%(title)s.%(ext)s'),
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+    }
+
+    try:
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([request.url])
+        return {"message": "Download completed", "location": downloads_folder}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error downloading video: {str(e)}")
