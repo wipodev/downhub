4509c85a3e6925d63cc38c95e5394e654fd975c5
wipodev <ajwipo@gmail.com>
2024-12-20 01:09:29 -0500

    added script for auto-execution and installed pyinstaller to package the backend for windows environments without python installed

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,17 @@
 # python modules
 import os
+import sys
 
 # third-party modules
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from yt_dlp import YoutubeDL
+import logging
+from uvicorn import Config, Server
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(current_dir)
 
 app = FastAPI()
 
@@ -36,3 +42,16 @@ def download_video(request: VideoRequest):
         return {"message": "Download completed", "location": downloads_folder}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error downloading video: {str(e)}")
+
+if __name__ == "__main__":
+    logger = logging.getLogger("uvicorn")
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("%(levelname)s: %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+    config = Config(app=app, host="127.0.0.1", port=8000, log_level="info", use_colors=False)
+    server = Server(config)
+
+    server.run()
